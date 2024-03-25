cdc86e5a29333bddebe42cd739b2739863d1a648
proofofjogi <ingokonito@protonmail.com>
2024-03-25 18:21:43 +0100

    rpc established

--- backend/utils/get_fees.py
+++ backend/utils/get_fees.py
@@ -1,11 +1,24 @@
 # get fees from an RPC connection
 from dotenv import dotenv_values
+from bitcoinrpc import BitcoinRPC
+import asyncio
 
 # Load environment variables from .env file
 env_vars = dotenv_values('.env')
 
+
+
 # Get RPC credentials from environment variables
 rpc_user = env_vars.get('RPC_USER')
-rpc_password = env_vars.get('RPC_PASSWORD')
+rpc_passwd = env_vars.get('RPC_PASSWORD')
 
-print(rpc_user, rpc_password)
+
+rpc = BitcoinRPC.from_config("http://localhost:8332", (rpc_user, rpc_passwd))
+
+
+async def main():
+    async with rpc:
+        print(await rpc.getconnectioncount())
+
+if __name__ == "__main__":
+    asyncio.run(main())
