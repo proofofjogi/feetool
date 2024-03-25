df231faf7f1a710c71e841b655a139e531e00ed7
proofofjogi <ingokonito@protonmail.com>
2024-03-25 19:00:12 +0100

    x

--- backend/__TODO.js
+++ backend/__TODO.js
@@ -3,16 +3,17 @@
 // *colors via better comments extension
 
 // !Inplement fee cronjob
-//    Establish RCP connection to core on device.
-//    get fees from core
+// //    Establish RCP connection to core on device.
+// //    get fees from core
 //    store fees pocketbase table
 //    language: python
 //    run server cronjob regularly (every 5 minutes?)
-//    * place in backend/utils/get_fees.py
+// //    * place in backend/utils/get_fees.py
 
 // !Spin up a pocketbase backend
-//    host at pb.domain.tld via Caddy
+//    host at localhost
 //    make a collection for fees
 //       priorities high low medium
 //       ? something else?
 //   * place in backend/pb/...
+// !OR CONSIDER USING A SIMPLE CSV FILE, less overhead
