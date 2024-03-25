7e9a802048dcfdf83fe7de4ddece8f1438ddf038
proofofjogi <ingokonito@protonmail.com>
2024-03-25 17:25:05 +0100

    backend specs

---
+++ backend/TODO.js
@@ -0,0 +1,18 @@
+// TODO list for the backend
+// *this is a JS file with comments so that VS Code shows
+// *colors via better commentsextension
+
+// !Inplement fee cronjob
+//    Establish RCP connection to core on device.
+//    get fees from core
+//    store fees pocketbase table
+//    language: python
+//    run server cronjob regularly (every 5 minutes?)
+//    * place in backend/utils/get_fees.py
+
+// !Spin up a pocketbase backend
+//    host at pb.domain.tld via Caddy
+//    make a collection for fees
+//       priorities high low medium
+//       ? something else?
+//   * place in backend/pb/...
