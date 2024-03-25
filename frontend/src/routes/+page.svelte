96b8e85a135dba675c5de30acdc160005a4e9014
proofofjogi <ingokonito@protonmail.com>
2024-03-25 17:36:04 +0100

    script, head tags

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+	// script initialized
+	// export let data
+</script>
+
+<svelte:head>
+	<!-- !FOR PWA -->
+</svelte:head>
+
 <h1>Welcome to Fee Tool</h1>
 <p>Svelte kit installed</p>
