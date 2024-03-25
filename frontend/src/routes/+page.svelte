853e02f33ff75c6a5544f2499ac6bdd0bb3a022b
proofofjogi <ingokonito@protonmail.com>
2024-03-25 17:49:20 +0100

    styling tweaks

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,5 +7,13 @@
 	<!-- !FOR PWA -->
 </svelte:head>
 
-<h1>Welcome to Fee Tool</h1>
-<p>Svelte kit installed</p>
+<!-- Take full height (mobile first), center, is simple SPA anyway -->
+<main class="h-[100dvh] flex justify-center items-center">
+	<div class="flex flex-col gap-3">
+		<h1 class="text-center text-4xl font-bold text-primary">Fee Tool</h1>
+		<p class="text-center text-xl">a bitcoin transaction fee tool</p>
+		<div>
+			<p class="text-center">DISPLAY THE DATA HERE</p>
+		</div>
+	</div>
+</main>
