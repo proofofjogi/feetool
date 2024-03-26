84aa5ffb211109aa46e2c52905e8976960521713
proofofjogi <ingokonito@protonmail.com>
2024-03-26 18:55:25 +0100

    backend todo

--- backend/__TODO.js
+++ backend/__TODO.js
@@ -5,15 +5,16 @@
 // !Inplement fee cronjob
 // //    Establish RCP connection to core on device.
 // //    get fees from core
-//    store fees pocketbase table
-//    language: python
+// //   store fees pocketbase table
+// //   language: python
 //    run server cronjob regularly (every 5 minutes?)
+// change unit to be sat/vbyte
+// default unit is btc/kbyte accoring to https://developer.bitcoin.org/reference/rpc/estimatesmartfee.html
 // //    * place in backend/utils/get_fees.py
 
 // !Spin up a pocketbase backend
-//    host at localhost
-//    make a collection for fees
-//       priorities high low medium
-//       ? something else?
+// //    host at localhost
+// //   make a collection for fees
+// //     priorities high low medium
+// //      ? something else?
 //   * place in backend/pb/...
-// !OR CONSIDER USING A SIMPLE CSV FILE, less overhead
